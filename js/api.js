5767dd2a513f74ee5ead68ece16bd56990586bfb
Kerveland Richard <r.ikerveland@gmail.com>
2019-01-31 15:04:46 +0000

    added git ignore file

--- js/api.js
+++ js/api.js
@@ -1,32 +1,35 @@
-// https://www.nytimes.com/?0751ffff01d7a70710354972fa0ad4a9
 
-$('#option').on('change', function() {
-   
+
+$('#option').on('change', function () {
+
     $.ajax({
-      method: 'GET',
-      url: 'https://api.nytimes.com/svc/topstories/v2/'+$('#option').val()+'.json?api-key=0751ffff01d7a70710354972fa0ad4a9'
+        method: 'GET',
+        url: 'https://api.nytimes.com/svc/topstories/v2/' + $('#option').val() + '.json?api-key=0751ffff01d7a70710354972fa0ad4a9'
     })
-    
-    .done(function(data) {
-        // console.log(data)
-        $('.h').empty()
-         for (i = 0; i <=12; i++) {
-         console.log(data.results);
-        var description = data.results[i].abstract
-        var imageUrl= data.results[i].multimedia[4].url
-        var articleLink = data.results[i].url
-        console.log(description)
-         console.log(imageUrl)
-        console.log(articleLink)
 
-         var htmlString = '<div class="articlecontainer"><img  src="' + imageUrl + '" /> <h2>' + description + '</h2><h3>' + articleLink + '</h3></div>';
-        console.log(htmlString);
+        .done(function (data) {
+            // console.log(data)
+            $('.h').empty()
+            for (i = 0; i <= 12; i++) {
+                console.log(data.results);
+                var description = data.results[i].abstract
+                var imageUrl = data.results[i].multimedia[4].url
+                var articleLink = data.results[i].url
+                console.log(description)
+                console.log(imageUrl)
+                console.log(articleLink)
+
+                // var htmlString = '<a class="textColor" href="'+articleLink+'"><div class="articleContainer"><h1>' + description + '</h1></div></a>'
+                //  console.log(htmlString);
+
+                var htmlString = '<div class="articlecontainer"><img  src="' + imageUrl + '" /> <h2>' + description + '</h2><h3>' + articleLink + '</h3></div>';
+                console.log(htmlString);
 
 
-         $('.h').append(htmlString);
+                $('.h').append(htmlString);
 
-         }
-
-    });
+            }
+            $('myObject').css('background-image', 'url(' + imageUrl + ')');
+        });
 });
 
