a1bdf6ac4db809b36452d0897436578eb6c37f1c
Kerveland Richard <r.ikerveland@gmail.com>
2019-02-01 10:20:29 +0000

    insta-news updated anthe url is now working but much work needed

--- js/api.js
+++ js/api.js
@@ -1,35 +1,36 @@
-
-
-$('#option').on('change', function () {
+$('#option').on('change', function() {
+    event.preventDefault();
+console.log("here");
 
     $.ajax({
         method: 'GET',
-        url: 'https://api.nytimes.com/svc/topstories/v2/' + $('#option').val() + '.json?api-key=0751ffff01d7a70710354972fa0ad4a9'
+        url: 'https://api.nytimes.com/svc/topstories/v2/' + $('#option').val() + '.json?api-key=0751ffff01d7a70710354972fa0ad4a9',
+        dataType: "json"
     })
 
-        .done(function (data) {
+        .done(function(data) {
             // console.log(data)
-            $('.h').empty()
+            $('.news-container').empty();
             for (i = 0; i <= 12; i++) {
                 console.log(data.results);
-                var description = data.results[i].abstract
-                var imageUrl = data.results[i].multimedia[4].url
-                var articleLink = data.results[i].url
-                console.log(description)
-                console.log(imageUrl)
-                console.log(articleLink)
+                var description = data.results[i].abstract;
+                var imageUrl = data.results[i].multimedia[4].url;
+                var articleLink = data.results[i].url;
+                console.log(description);
+                console.log(articleLink);
 
-                // var htmlString = '<a class="textColor" href="'+articleLink+'"><div class="articleContainer"><h1>' + description + '</h1></div></a>'
-                //  console.log(htmlString);
 
-                var htmlString = '<div class="articlecontainer"><img  src="' + imageUrl + '" /> <h2>' + description + '</h2><h3>' + articleLink + '</h3></div>';
+                var htmlString = '<a target="_blank" href=" ' + articleLink + ' "><div class="articleContainer"><h2>' + description + '</h2></div></a>';
                 console.log(htmlString);
 
 
-                $('.h').append(htmlString);
+                $('.news-container').append(htmlString);
+                console.log("here");
 
+            
+            $('.articleContainer').last().css('background', 'url(' + imageUrl + ')');
+            console.log("here as well");
             }
-            $('myObject').css('background-image', 'url(' + imageUrl + ')');
-        });
+        })
 });
 
